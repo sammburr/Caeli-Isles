9d404dd83b6e6ae1f8f99919e815329b4d650612
sammburr <53175463+sammburr@users.noreply.github.com>
2023-12-14 16:37:05 +0000

    Working on wall placement
    Not currently working!

--- Construction/Building Manager.gd
+++ Construction/Building Manager.gd
@@ -2,9 +2,12 @@ extends Node
 
 @export var KEY_BIND_PLACE := "left click"
 @export var KEY_BIND_BUILD_START := "build"
+@export var KEY_BIND_BUILD_SELECT_WHEEL := "select"
 
 @export var GHOST_FOUNDATION : PackedScene
 @export var FOUNDATION : PackedScene
+@export var WALL : PackedScene
+@export var GHOST_WALL : PackedScene
 
 @export var PLAYER : CharacterBody3D
 
@@ -14,6 +17,8 @@ var can_place = false
 
 # Ghost copy to indicate placment
 var ghost : Node3D
+# Construction to place
+var construct : Node3D
 
 # Ray cast hit
 var hit
@@ -31,6 +36,9 @@ func _ready():
 	ghost.hide()
 	# Parent it to the root of the game
 	get_window().add_child.call_deferred(ghost)
+	$"Building UI".hide()
+	# Set default construction to foundation
+	construct = FOUNDATION.instantiate()
 
 func _input(event):
 	if event.is_action_pressed(KEY_BIND_BUILD_START):
@@ -39,12 +47,31 @@ func _input(event):
 	if event.is_action_pressed(KEY_BIND_PLACE) && can_place:
 		build()
 
+	# Only open select wheel when we are already building
+	if event.is_action_pressed(KEY_BIND_BUILD_SELECT_WHEEL) && building:
+		open_wheel()
+	
+	if event.is_action_released(KEY_BIND_BUILD_SELECT_WHEEL):
+		close_wheel()
+
 func _process(delta):
 	if building:
 		place_ghost()
 	else:
 		can_place = false
 		ghost.hide()
+
+func open_wheel():
+	# Show UI
+	$"Building UI".show()
+	# Show mouse
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+func close_wheel():
+	# Hide UI
+	$"Building UI".hide()
+	# Capture mouse
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func place_ghost():
 	# Place a ghost on the floor to see where a construction is placed
@@ -77,9 +104,22 @@ func check_valid(hit):
 	return !ghost.PLACEMENT.has_overlapping_bodies()
 
 func build():
-	# Make a new instance of the ghost, set its parent to root
-	var construct = FOUNDATION.instantiate()
-	get_window().add_child.call_deferred(construct)
+	# Place construct
+	get_window().add_child.call_deferred(construct.duplicate())
 	# Copy the position and rotation of the ghost
 	construct.position = ghost.global_position 
 	construct.rotation = ghost.rotation
+
+func _on_wall_mouse_entered():
+	# Set current construction and ghost to wall
+	construct = WALL.instantiate()
+	
+	get_window().remove_child.call_deferred(ghost)	
+	ghost = GHOST_WALL.instantiate()
+	get_window().add_child.call_deferred(ghost)
+	
+func _on_foundation_mouse_entered():
+	pass
+	# Set current  construction and ghost to foundation
+	# construct = FOUNDATION.instantiate()
+	# ghost = GHOST_FOUNDATION.instantiate()
